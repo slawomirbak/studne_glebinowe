abe0bb927240a7e5f892bf82ef7d85a7eaf00d22
Sławomir Bąk <slawek417@gmail.com>
2019-03-16 21:50:53 +0100

    initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,6 @@
+$('.anchor-element').click(function(e){
+    e.preventDefault();
+    var jumpId =$(this).find('a:first').attr('href')
+    // leave 50 px from top
+    $('body, html').animate({scrollTop: $(jumpId).offset().top-50}, 'slow');
+});
