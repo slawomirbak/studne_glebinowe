ce414c8bb342e24b415a4f2c6e4a0ad9c3166dbc
Sławomir Bąk <slawek417@gmail.com>
2019-03-22 21:46:28 +0100

    add script to turn off auto scroll map

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,3 +4,8 @@ $('.anchor-element').click(function(e){
     // leave 50 px from top
     $('body, html').animate({scrollTop: $(jumpId).offset().top-50}, 'slow');
 });
+
+$('.overlay-map').click(function(e){
+    e.preventDefault();
+    $(this).css("display","none");
+});
